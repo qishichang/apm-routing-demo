6396bcbeda37f61bcaca88c682975f6c3cad7f7d
qishichang <qishichang@live.com>
2019-10-16 23:50:07 +1300

    Config child routes

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -5,6 +5,8 @@ import { ProductListComponent } from './product-list.component';
 import { ProductDetailComponent } from './product-detail.component';
 import { ProductEditComponent } from './product-edit/product-edit.component';
 import { ProductResolver } from './product-resolver.service';
+import { ProductEditInfoComponent } from './product-edit/product-edit-info.component';
+import { ProductEditTagsComponent } from './product-edit/product-edit-tags.component';
 
 import { SharedModule } from '../shared/shared.module';
 
@@ -19,14 +21,27 @@ import { SharedModule } from '../shared/shared.module';
       },
       { path: 'products/:id/edit',
         component: ProductEditComponent,
-        resolve: { product: ProductResolver}
+        resolve: { product: ProductResolver},
+        children: [
+          {
+            path: '', redirectTo: 'info', pathMatch: 'full'
+          },
+          {
+            path: 'info', component: ProductEditInfoComponent
+          },
+          {
+            path: 'tags', component: ProductEditTagsComponent
+          }
+        ]
       }
     ])
   ],
   declarations: [
     ProductListComponent,
     ProductDetailComponent,
-    ProductEditComponent
+    ProductEditComponent,
+    ProductEditInfoComponent,
+    ProductEditTagsComponent
   ]
 })
 export class ProductModule { }
