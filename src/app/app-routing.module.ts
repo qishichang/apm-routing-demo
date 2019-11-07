4b75ac860bcf6e59386213ccc02cbe240e75196a
qishichang <qishichang@live.com>
2019-11-07 15:47:32 +1300

    Preload freature modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
 import { WelcomeComponent } from './home/welcome.component';
 import { PageNotFoundComponent } from './page-not-found.component';
@@ -8,7 +8,7 @@ import { AuthGuard } from './user/auth.guard';
 const routes: Routes = [
   { path: 'welcome', component: WelcomeComponent },
   { path: 'products',
-    canLoad: [ AuthGuard ],
+    canActivate: [ AuthGuard ],
     loadChildren: () =>
       import('./products/product.module').then(m => m.ProductModule)},
   { path: '', redirectTo: 'welcome', pathMatch: 'full'},
@@ -17,7 +17,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { enableTracing: true })
+    RouterModule.forRoot(routes, { enableTracing: true, preloadingStrategy: PreloadAllModules })
   ],
   exports: [
     RouterModule
